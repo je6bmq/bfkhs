5e885a8bc9156ff6ed5780c707fc0df834fd8d29
matchaBread <12829065+matchaBread@users.noreply.github.com>
2021-06-13 12:53:50 +0000

    [parser] list -> vector

--- src/Data/Bfk/Parser.hs
+++ src/Data/Bfk/Parser.hs
@@ -1,9 +1,10 @@
 module Data.Bfk.Parser (parseInstructions, BfkInstruction (..), ParseInstructionError (..)) where
 
 import Control.Applicative
+import qualified Data.Vector as V
 
 -- Brain**kの命令
-data BfkInstruction = IncrPtr | DecrPtr | IncrValue | DecrValue | OutputValue | ReadValue | Loop [BfkInstruction] deriving (Show, Eq)
+data BfkInstruction = IncrPtr | DecrPtr | IncrValue | DecrValue | OutputValue | ReadValue | Loop (V.Vector BfkInstruction) deriving (Show, Eq)
 
 data ParseInstructionError = InvalidCharacter Char Int | UnclosedDelimiter Int deriving (Eq) -- エラー定義。
 
@@ -11,23 +12,23 @@ instance Show ParseInstructionError where
   show (InvalidCharacter c i) = c : " is invalid character at " ++ show i ++ "."
   show (UnclosedDelimiter p) = "unclosed delimiter near " ++ show (p + 1)
 
-parseInstructions :: String -> Either ParseInstructionError [BfkInstruction]
-parseInstructions program = parseInstructionsImpl 0 program $ Right []
+parseInstructions :: String -> Either ParseInstructionError (V.Vector BfkInstruction)
+parseInstructions program = parseInstructionsImpl 0 program $ Right V.empty
   where
-    parseInstructionsImpl :: Int -> String -> Either (Int -> ParseInstructionError) [BfkInstruction] -> Either ParseInstructionError [BfkInstruction]
+    parseInstructionsImpl :: Int -> String -> Either (Int -> ParseInstructionError) (V.Vector BfkInstruction) -> Either ParseInstructionError (V.Vector BfkInstruction)
     parseInstructionsImpl _ [] (Right insts) = Right insts
     parseInstructionsImpl column [] (Left errFunc) = Left $ errFunc column
     parseInstructionsImpl column (c : program) insts =
       case c of
-        '[' -> case parseLoopInstruction program $ Right [] of -- 直前の命令がループでなく、ループ開始時に場合に呼ばれる
+        '[' -> case parseLoopInstruction program $ Right V.empty of -- 直前の命令がループでなく、ループ開始時に場合に呼ばれる
           Right (remainedProgram, loopInsts) ->
             let loop = Right $ Loop loopInsts
-                newInsts = (++) <$> insts <*> sequence [loop]
+                newInsts = (V.++) <$> insts <*> sequence (V.singleton loop)
              in parseInstructionsImpl (column + (length program - length remainedProgram)) remainedProgram newInsts
           Left errFunc -> Left $ errFunc column
         ']' -> Left $ UnclosedDelimiter column
         _ -> case parseUnaryInstruction c of
-          Right inst -> parseInstructionsImpl (column + 1) program $ (++) <$> insts <*> sequence [Right inst]
+          Right inst -> parseInstructionsImpl (column + 1) program $ (V.++) <$> insts <*> sequence (V.singleton $ Right inst)
           Left errFunc -> Left $ errFunc column
     parseUnaryInstruction :: Char -> Either (Int -> ParseInstructionError) BfkInstruction -- 単項演算子群のパース
     parseUnaryInstruction '>' = Right IncrPtr
@@ -37,10 +38,10 @@ parseInstructions program = parseInstructionsImpl 0 program $ Right []
     parseUnaryInstruction '.' = Right OutputValue
     parseUnaryInstruction ',' = Right ReadValue
     parseUnaryInstruction s = Left $ InvalidCharacter s -- 上どれにもマッチしない場合. ループは別処理なのでここにはまらない
-    parseLoopInstruction :: String -> Either (Int -> ParseInstructionError) [BfkInstruction] -> Either (Int -> ParseInstructionError) (String, [BfkInstruction])
+    parseLoopInstruction :: String -> Either (Int -> ParseInstructionError) (V.Vector BfkInstruction) -> Either (Int -> ParseInstructionError) (String, (V.Vector BfkInstruction))
     parseLoopInstruction [] _ = Left UnclosedDelimiter
-    parseLoopInstruction ('[' : program) insts = case parseLoopInstruction program $ Right [] of -- ループ処理パース中に子ループに突入する場合に呼ばれる
-      Right (remainedProgram, loopInsts) -> parseLoopInstruction remainedProgram $ (++) <$> insts <*> Right [Loop loopInsts] -- これまで得られた命令列(insts)にloop処理 loopInstsをconcatする
+    parseLoopInstruction ('[' : program) insts = case parseLoopInstruction program $ Right V.empty of -- ループ処理パース中に子ループに突入する場合に呼ばれる
+      Right (remainedProgram, loopInsts) -> parseLoopInstruction remainedProgram $ (V.++) <$> insts <*> Right (V.singleton $ Loop loopInsts) -- これまで得られた命令列(insts)にloop処理 loopInstsをconcatする
       Left s -> Left s
     parseLoopInstruction (']' : program) (Right insts) = sequence (program, Right insts) -- parseLoopInstructionsの終了条件
-    parseLoopInstruction (c : cs) insts = parseLoopInstruction cs $ (++) <$> insts <*> sequence [parseUnaryInstruction c]
+    parseLoopInstruction (c : cs) insts = parseLoopInstruction cs $ (V.++) <$> insts <*> sequence (V.singleton $ parseUnaryInstruction c)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,5 @@
 import Data.Bfk.Parser
+import qualified Data.Vector as V
 import Test.HUnit
 
 main :: IO ()
@@ -11,31 +12,31 @@ main = do
 parseUnaryInstructionsTest :: Test
 parseUnaryInstructionsTest =
   TestList
-    [ "parse simple increment pointer" ~: parseInstructions ">" ~?= Right [IncrPtr],
-      "parse simple decrement pointer" ~: parseInstructions "<" ~?= Right [DecrPtr],
-      "parse simple increment value" ~: parseInstructions "+" ~?= Right [IncrValue],
-      "parse simple decrement value" ~: parseInstructions "-" ~?= Right [DecrValue],
-      "parse simple output value" ~: parseInstructions "." ~?= Right [OutputValue],
-      "parse simple read pointer" ~: parseInstructions "," ~?= Right [ReadValue],
-      "parse lough multiple instructions" ~: parseInstructions "><+-.," ~?= Right [IncrPtr, DecrPtr, IncrValue, DecrValue, OutputValue, ReadValue]
+    [ "parse simple increment pointer" ~: parseInstructions ">" ~?= Right (V.singleton IncrPtr),
+      "parse simple decrement pointer" ~: parseInstructions "<" ~?= Right (V.singleton DecrPtr),
+      "parse simple increment value" ~: parseInstructions "+" ~?= Right (V.singleton IncrValue),
+      "parse simple decrement value" ~: parseInstructions "-" ~?= Right (V.singleton DecrValue),
+      "parse simple output value" ~: parseInstructions "." ~?= Right (V.singleton OutputValue),
+      "parse simple read pointer" ~: parseInstructions "," ~?= Right (V.singleton ReadValue),
+      "parse lough multiple instructions" ~: parseInstructions "><+-.," ~?= Right (V.fromList [IncrPtr, DecrPtr, IncrValue, DecrValue, OutputValue, ReadValue])
     ]
 
 parsePairInstructionsTest =
   TestList
     [ "prase only left bracket (error)" ~: parseInstructions "[" ~?= Left (UnclosedDelimiter 0),
       "parse only right bracket (error)" ~: parseInstructions "]" ~?= Left (UnclosedDelimiter 0),
-      "parse simple bracket" ~: parseInstructions "[]" ~?= Right [Loop []],
-      "parsenested bracket" ~: parseInstructions "[[[]]]" ~?= Right [Loop [Loop [Loop []]]],
-      "parsemultiple bracket" ~: parseInstructions "[][][]" ~?= Right [Loop [], Loop [], Loop []],
+      "parse simple bracket" ~: parseInstructions "[]" ~?= Right (V.singleton (Loop V.empty)),
+      "parsenested bracket" ~: parseInstructions "[[[]]]" ~?= Right (V.singleton (Loop (V.singleton (Loop $ V.singleton (Loop V.empty))))),
+      "parsemultiple bracket" ~: parseInstructions "[][][]" ~?= Right (V.fromList [Loop V.empty, Loop V.empty, Loop V.empty]),
       "parseinside left bracket (error)" ~: parseInstructions "[[]" ~?= Left (UnclosedDelimiter 0),
       "parse inside right bracket (error)" ~: parseInstructions "[]]" ~?= Left (UnclosedDelimiter 1)
     ]
 
 parseComplicatedInstructionsTest =
   TestList
-    [ "parse complicated test 1" ~: parseInstructions "[.,[+-]]" ~?= Right [Loop [OutputValue, ReadValue, Loop [IncrValue, DecrValue]]],
-      "parse complicated test 2" ~: parseInstructions "[[+-]><]" ~?= Right [Loop [Loop [IncrValue, DecrValue], IncrPtr, DecrPtr]],
-      "parse complicated test 3" ~: parseInstructions "[+-[>-.-<]]" ~?= Right [Loop [IncrValue, DecrValue, Loop [IncrPtr, DecrValue, OutputValue, DecrValue, DecrPtr]]],
-      "parse complicated test 4" ~: parseInstructions "[+-[>-.-<][]]" ~?= Right [Loop [IncrValue, DecrValue, Loop [IncrPtr, DecrValue, OutputValue, DecrValue, DecrPtr], Loop []]],
-      "parse complicated test 5" ~: parseInstructions "[>[>[-.-]<]<]" ~?= Right [Loop [IncrPtr, Loop [IncrPtr, Loop [DecrValue, OutputValue, DecrValue], DecrPtr], DecrPtr]]
+    [ "parse complicated test 1" ~: parseInstructions "[.,[+-]]" ~?= Right (V.singleton (Loop $ V.fromList [OutputValue, ReadValue, Loop (V.fromList [IncrValue, DecrValue])])),
+      "parse complicated test 2" ~: parseInstructions "[[+-]><]" ~?= Right (V.fromList [Loop (V.fromList [Loop $ V.fromList [IncrValue, DecrValue], IncrPtr, DecrPtr])]),
+      "parse complicated test 3" ~: parseInstructions "[+-[>-.-<]]" ~?= Right (V.fromList [Loop $ V.fromList [IncrValue, DecrValue, Loop (V.fromList [IncrPtr, DecrValue, OutputValue, DecrValue, DecrPtr])]]),
+      "parse complicated test 4" ~: parseInstructions "[+-[>-.-<][]]" ~?= Right (V.fromList [Loop (V.fromList [IncrValue, DecrValue, Loop (V.fromList [IncrPtr, DecrValue, OutputValue, DecrValue, DecrPtr]), Loop V.empty])]),
+      "parse complicated test 5" ~: parseInstructions "[>[>[-.-]<]<]" ~?= Right (V.fromList [Loop (V.fromList [IncrPtr, Loop (V.fromList [IncrPtr, Loop (V.fromList [DecrValue, OutputValue, DecrValue]), DecrPtr]), DecrPtr])])
     ]
